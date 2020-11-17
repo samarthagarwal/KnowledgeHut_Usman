573cf58bb93397c52de401b9f076c861b97d3d28
Samarth Agarwal <samarthagarwal@live.com>
2020-11-17 22:42:54 +0530

    Bindings and Interpolation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,37 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  pageTitle: string = "HelloIonic"; //String
+  num1: number = 45; //int, double, float
+  num2: number = 12; //int, double, float
+  isValid: boolean = true; // bool
+  buttonColor: string = "danger";
+
+  constructor() {
+    
+  }
+
+  ionViewWillEnter() {
+    console.log("Page is about to be loaded...");
+  }
+
+  ionViewDidEnter() {
+    console.log("Page is loaded...");
+  }
+
+  ionViewWillLeave() {
+    console.log("Page is about to be unloaded...");
+  }
+
+  ionViewDidLeave() {
+    console.log("Page is unloaded...");
+  }
+
+  changeColor() {
+    if(this.buttonColor == "danger")
+      this.buttonColor = "success";
+     else 
+     this.buttonColor = "danger";
+  }
 
 }
